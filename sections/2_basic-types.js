4b8c934e1020e60e31ac9b9e93a333ea233de16a
Luis <vsjoseluisvs@gmail.com>
2022-11-30 21:58:35 -0600

    basic types

---
+++ sections/2_basic-types.js
@@ -0,0 +1,39 @@
+// Correr nodemon :)
+// string
+
+let firstName = 'Patricio';
+let lastName  = 'Estrella';
+
+/**
+ * Los console.log son solo para mostrar logs (mostrar la consola con nodemon y explicar que esos son logs)
+ */
+console.log(firstName, lastName);
+
+let fullName = firstName + lastName; // <- Concatenación de strings 
+
+console.log(fullname); // <- preguntar porque aparece un error y que lo corrigan
+
+console.log(fullName); // El string aparecea todo junto(PatricioEstrella), explicarles porque y agregar un espacio
+
+// number
+let number1 = 10; // Tambien pueden ser decimales, negativos, etc...
+let number2 = 10; 
+
+/**
+ * Explicar las operaciones matematicas y que JS sabe que son numeros por eso realiza las sumas :)
+ */
+console.log(number1 + number2);
+
+// boolean
+
+let isActive = true;
+let isRunning = false;
+let hasBoyfriend = true;
+
+/**
+ * Imprimir en consola y explicar porque la variable aparece como true, ya que es el valor que contiene
+ */
+
+console.log('isActive', isActive); // Explicar un poquito sobre los parametros
+console.log('isRunning', isRunning);
+console.log('hasBoyfriend', hasBoyfriend);
